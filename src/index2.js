01048208a03fb1cf84824c602d10a15dbdef48a6
ProfesorTenebro <ghost.intimidation+github@gmail.com>
2020-05-01 05:46:51 +0200

    Probandolo en index2.js

---
+++ src/index2.js
@@ -0,0 +1,22 @@
+const Link = ReactRouterDOM.Link,
+Route = ReactRouterDOM.Route;
+
+const App = props => (
+<ReactRouterDOM.HashRouter>
+<ul>
+<li><Link to="/">TO HOME</Link></li>
+<li><Link to="/a">TO A</Link></li>
+<li><Link to="/b">TO B</Link></li>
+</ul>
+
+<Route path="/" exact component={Home} />
+<Route path="/a" component={A} />
+<Route path="/b" component={B} />
+</ReactRouterDOM.HashRouter>
+)
+
+const Home = props => <h1>HOME</h1>
+const A = props => <h1>A</h1>
+const B = props => <h1>B</h1>
+
+ReactDOM.render(<App />, document.querySelector('#root'));
